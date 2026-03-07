d250e7332797e71e7e0c040829b3e3fd75b2d62d
DESKTOP-ONPJEMR\USER <nishikaemalshi@gmail.com>
2026-03-07 13:17:28 +0530

    mearge with main and wrte sense and add to dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type JSX } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import EyeTrackingBoot from "./components/EyeTrackingBoot";
 import RoleSelection from "./pages/RoleSelection";
@@ -14,6 +14,10 @@ import ShapeNinja from "./pages/ninjagame";
 import AmbloCar from "./pages/AmbloCar";
 import SnakeGame from "./pages/snake";
 import ParentsDashboard from "./pages/ParentsDashboard";
+import LandingPage from "./pages/writesense/Landing";
+import Tutorials from "./pages/writesense/Tutorials";
+import Quiz from "./pages/writesense/Quiz";
+import Results from "./pages/writesense/Result";
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
   const userRole = localStorage.getItem("userRole");
@@ -26,17 +30,68 @@ const App: React.FC = () => (
     <Routes>
       <Route path="/role-selection" element={<RoleSelection />} />
       <Route path="/" element={<ProtectedRoute element={<Home />} />} />
-      <Route path="/eye-problem-detector" element={<ProtectedRoute element={<EyeProblemDetector />} />} />
-      <Route path="/vision-therapy" element={<ProtectedRoute element={<VisionTherapy />} />} />
-      <Route path="/games" element={<ProtectedRoute element={<GamesPage />} />} />
-      <Route path="/games/amblocar" element={<ProtectedRoute element={<AmbloCar />} />} />
-      <Route path="/games/ninjagame" element={<ProtectedRoute element={<ShapeNinja />} />} />
-      <Route path="/games/snakegame" element={<ProtectedRoute element={<SnakeGame />} />} />
-      <Route path="/shapes/circle" element={<ProtectedRoute element={<CircleShape />} />} />
-      <Route path="/shapes/square" element={<ProtectedRoute element={<SquareShape />} />} />
-      <Route path="/shapes/triangle" element={<ProtectedRoute element={<TriangleShape />} />} />
-      <Route path="/shapes/star" element={<ProtectedRoute element={<StarShape />} />} />
-      <Route path="/parents-dashboard" element={<ProtectedRoute element={<ParentsDashboard />} />} />
+      <Route
+        path="/eye-problem-detector"
+        element={<ProtectedRoute element={<EyeProblemDetector />} />}
+      />
+      <Route
+        path="/vision-therapy"
+        element={<ProtectedRoute element={<VisionTherapy />} />}
+      />
+      <Route
+        path="/games"
+        element={<ProtectedRoute element={<GamesPage />} />}
+      />
+      <Route
+        path="/games/amblocar"
+        element={<ProtectedRoute element={<AmbloCar />} />}
+      />
+      <Route
+        path="/games/ninjagame"
+        element={<ProtectedRoute element={<ShapeNinja />} />}
+      />
+      <Route
+        path="/games/snakegame"
+        element={<ProtectedRoute element={<SnakeGame />} />}
+      />
+      <Route
+        path="/shapes/circle"
+        element={<ProtectedRoute element={<CircleShape />} />}
+      />
+      <Route
+        path="/shapes/square"
+        element={<ProtectedRoute element={<SquareShape />} />}
+      />
+      <Route
+        path="/shapes/triangle"
+        element={<ProtectedRoute element={<TriangleShape />} />}
+      />
+      <Route
+        path="/shapes/star"
+        element={<ProtectedRoute element={<StarShape />} />}
+      />
+      <Route
+        path="/parents-dashboard"
+        element={<ProtectedRoute element={<ParentsDashboard />} />}
+      />
+
+      {/* Add more routes as write sense*/}
+      <Route
+        path="/write-sense/landing-page"
+        element={<ProtectedRoute element={<LandingPage />} />}
+      />
+      <Route
+        path="/write-sense/tutorials"
+        element={<ProtectedRoute element={<Tutorials />} />}
+      />
+      <Route
+        path="/write-sense/quiz"
+        element={<ProtectedRoute element={<Quiz />} />}
+      />
+      <Route
+        path="/write-sense/results"
+        element={<ProtectedRoute element={<Results />} />}
+      />
     </Routes>
   </>
 );
